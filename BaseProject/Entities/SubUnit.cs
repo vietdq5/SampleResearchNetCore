1f62220211e863fd96e66ed24134918b5cc1f14f
Viet Demo <vietdq1@outlook.com>
2024-07-27 23:50:26 +0700

    #add Init Sample Project

---
+++ BaseProject/Entities/SubUnit.cs
@@ -0,0 +1,12 @@
+﻿namespace BaseProject.Entities;
+
+public class SubUnit : BaseEntities
+{
+    public string? SubName { get; set; }
+    public IList<Employee> Employees { get; set; }
+
+    public SubUnit()
+    {
+        Employees = [];
+    }
+}
---
+++ BaseProject/Settings/MyConfigServiceCollectionExtensions.cs
@@ -0,0 +1,31 @@
+﻿using BaseProject.DataContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaseProject.Settings;
+
+public static class MyConfigServiceCollectionExtensions
+{
+    public static IServiceCollection AddConfig(
+            this IServiceCollection services, IConfiguration config)
+    {
+        services.Configure<PostgreOptions>(config.GetSection(PostgreOptions.PostgreOption));
+
+        return services;
+    }
+
+    public static IServiceCollection AddMyDBContext(
+        this IServiceCollection services,
+        IConfiguration config)
+    {
+        services.AddDbContext<ProjectDbContext>(option => option.UseNpgsql(config[$"{PostgreOptions.PostgreOption}:{nameof(PostgreOptions.LocalConnectionString)}"]));
+        return services;
+    }
+
+    public static IServiceCollection AddMyDependencyGroup(
+         this IServiceCollection services)
+    {
+        //services.AddScoped<IMyDependency, MyDependency>();
+
+        return services;
+    }
+}
